aba886fe1debc48673f7dc4a76407cf65e91540e
PhucNM <binguyenfpt1407@gmail.com>
2026-09-03 21:48:12 +0700

    add solution for LeetCode 20: Valid Parentheses

---
+++ LeetCode/20.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> stk;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s[i];
+            switch (c)
+            {
+            case '(': case '[': case '{':
+                stk.push(c);
+                break;
+            case '}':
+                if (stk.empty() == true || stk.top() != '{')
+                    return false;
+                stk.pop();
+                break;
+            case ')':
+                if (stk.empty() == true || stk.top() != '(')
+                    return false;
+                stk.pop();
+                break;
+            case ']':
+                if (stk.empty() == true || stk.top() != '[')
+                    return false;
+                stk.pop();
+                break;
+            }
+        }
+        return stk.empty();
+    }
+};
+
+int main() {
+
+    system("pause");
+    return 0;
+
+
+}
